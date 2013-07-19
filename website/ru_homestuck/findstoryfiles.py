41710c8b16926f2b5c97989438a1663659f56536
loonycyborg <loonycyborg@gmail.com>
2013-07-19 17:41:48 +0400

    Исправил collectstatic

--- website/ru_homestuck/findstoryfiles.py
+++ website/ru_homestuck/findstoryfiles.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.contrib.staticfiles import finders
+from django.contrib.staticfiles import utils
+from django.core.files.storage import FileSystemStorage
 import os.path
 from glob import glob
 
@@ -16,3 +18,13 @@ class StoryfilesFinder(finders.BaseFinder):
             return found_files
         else:
             return found_files[0]
+
+    def list(self, ignore_patterns):
+        storyfiles_dir = settings.HOMESTUCK_STORYFILES_DIR
+        dirs = glob(os.path.join(storyfiles_dir, "*", "img")) + \
+               glob(os.path.join(storyfiles_dir, "*", "*", "img"))
+        for directory in dirs:
+            storage = FileSystemStorage(location=directory)
+            storage.prefix = "storyfiles"
+            for path in utils.get_files(storage, ignore_patterns):
+               yield path, storage
