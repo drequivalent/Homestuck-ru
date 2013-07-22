9e497efe3ecd7cae0b3a53bb026b42bae2784926
loonycyborg <loonycyborg@gmail.com>
2013-07-22 17:40:36 +0400

    Исправил regex для dialoglog'ов

--- website/ru_homestuck/views.py
+++ website/ru_homestuck/views.py
@@ -21,7 +21,7 @@ def parse_txt(page_number):
 
     result = contents.split("###")
     result[5] = result[5].rstrip("\nX ").lstrip("\n")
-    m = re.match(r"^\n\|(.*)\|(.*)$", result[4], re.DOTALL)
+    m = re.match(r"^\n\|(.*?)\|(.*)$", result[4], re.DOTALL)
     if(m):
         result[4] = render_to_string("log.html", { 'name': m.group(1), 'text' : m.group(2).lstrip("\n").replace("\n", "\n<br />\n") })
     return result
